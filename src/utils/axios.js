afb2c87271d275cf8e79ec0f5e909547a398fead
Miller Adulu <milleradulu@gmail.com>
2019-03-15 13:19:04 +0300

    Add firebase hosting

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:4000/api/',
-  timeout: 1000,
+  baseURL: 'https://homesenseapi.herokuapp.com/api/',
+  timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
